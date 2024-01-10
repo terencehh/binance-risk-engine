e2337d48309797f38d78e10fdd89242e903e6f80
Terence Ng <ngtere@live.com.sg>
2024-01-10 13:08:07 +0800

    initial commit

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 Rationale
 
-Over 95% of traders lose money. I realized its not due to a lack of strategy that makes traders lose money, its usually the emotional sides of fear & greed which cause people to make stupid decisions like overtrading, overrisking on positions etc. Personally, I have experienced these struggles myself and its very hard to control some of these reasons due to human weakness of fear & greed. This project aims to address the risk side by ensuring a traders account can never blow up with a risk engine script.
+Over 95% of traders lose money. I realized its not due to a lack of strategy that makes traders lose money, its usually the emotional sides of fear & greed which cause people to make stupid decisions like overtrading, oversizing on positions etc, basically lack of risk management. Personally, I have experienced these struggles myself and its very hard to control some of these aspects due to human weakness of fear & greed. This project aims to address the risk side by ensuring a traders account can never blow up with a risk engine script that runs perpetually to ensure account drawdown never exceeds a certain threshold per trade.
+
+Setup
+
+Create a Binance Account + setup API key by creating a `.env` file mimicking `env.example`. Run the python script locally or deploy it online via a cloud server. Recommend to run perpetually for maximum effectiveness.
---
+++ script.py
@@ -0,0 +1,47 @@
+import os
+import time
+from dotenv import load_dotenv
+from binance import Client
+
+
+def main():
+    api_key = os.environ.get('BINANCE_API')
+    api_secret = os.environ.get('BINANCE_SECRET')
+    client = Client(api_key, api_secret)
+    risk_limit = 0.05  # 5% drawdown limit
+
+    # Poll every second
+    start_time = time.monotonic()
+    while True:
+        # fetch account balance in usdt
+        account_info = list(filter(lambda x: x['asset'] == 'USDT', client.futures_account_balance()))[0]
+        account_balance = float(account_info['balance'])
+        unrealized_pnl = float(account_info['crossUnPnl'])
+
+        print('Equity: {} - Unrealized Equity: {} - Drawdown Threshold: {}'.format(account_balance, account_balance + unrealized_pnl, account_balance * (1 - risk_limit)))
+
+        # drawdown breached, close all positions
+        if (account_balance + unrealized_pnl) < (account_balance * (1 - risk_limit)):
+            print('drawdown limit breached, closing all positions immediately')
+
+            # Fetch positions
+            position_info = list(filter(lambda x: float(x['positionAmt']) != 0, client.futures_position_information()))
+
+            # Iterate through positions and close them by placing opposite orders in the same amount
+            for position in position_info:
+                client.futures_create_order(
+                    symbol=position['symbol'],
+                    side='SELL' if float(position['positionAmt']) > 0 else 'BUY',
+                    type='MARKET',
+                    quantity=abs(float(position['positionAmt'])),
+                    reduceOnly="true",
+                )
+                print('Market Closed {} {} position'.format(position['symbol'], 'Long' if float(position['positionAmt']) > 0 else 'Short'))
+
+        # sleep until next second
+        time.sleep(1.0 - ((time.monotonic() - start_time) % 1.0))
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    main()
